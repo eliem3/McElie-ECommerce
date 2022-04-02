decfba1813a6833145fe73febd17412f8bd1f326
McElie <pachelimbuyamba>
2022-04-03 00:00:18 +0200

    stage-3 new update on the project. ready to publish the site

--- whatcamefirst/frontend/src/components/MessageBox.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Alert } from 'react-bootstrap';
-
-export default function MessageBox(props) {
-  return <Alert variant={props.variant || 'info'}>{props.children}</Alert>;
-}
